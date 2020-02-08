af00d1dac890e32ad17ea00de063df97b6b9d247
Glitch (hello-webpage) <none>
2020-02-08 15:33:42 +0000

    🌠💁 Checkpoint ./funding.html:11195082/349:11195110/4 ./server.py:8076444/209:11195115/37 ./index.html:8076444/553

--- server.py
+++ server.py
@@ -1,4 +1,13 @@
 
+import os
+import math
+from flask import Flask, request, render_template, jsonify
+
+# Support for gomix's 'front-end' and 'back-end' UI.
+app = Flask(__name__, static_folder='public', template_folder='views')
+
+
+
 x = int(input("How much are you planning on putting in your account?"))
 y = float(input("What is the rate of interest?(Input your rate in decimal form.)"))
 z = int(input("What is the amount of money you want to reach?"))
@@ -14,4 +23,4 @@ while x <= z:
   print(str(x) + "  at year " + str(n))
   x = round((x), 0)
   n += 1
-  x == x*e ** y*n
+  x == x* 2.7182818284590452353602874713527 ** y*n
