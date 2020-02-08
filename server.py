3931348a5b0df55e4ac6a4056b074ded7fc22fe9
Glitch (hello-webpage) <none>
2020-02-08 15:21:43 +0000

    📉🍶 Checkpoint ./funding.html:11195082/598:11195110/21 ./server.py:11195115/148

--- server.py
+++ server.py
@@ -1,13 +1,17 @@
 
 x = int(input("How much are you planning on putting in your account?"))
-y = int(input("What is the rate of interest?"))
+y = float(input("What is the rate of interest?(Input your rate in decimal form.)"))
 z = int(input("What is the amount of money you want to reach?"))
 n = 1 
 
 # Balance  =Pe^rt 
 # Balance = z P= x e= exponetial function rate= y time = n (years)
+
+import math
+x.append(1 - math.exp( -0.5 * (value1*value2)**2))
+
 while x <= z:
   print(str(x) + "  at year " + str(n))
   x = round((x), 0)
   n += 1
-  
+  x == x*e ** y*n
